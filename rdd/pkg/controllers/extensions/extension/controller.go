60a6f896e387d67aac0854f9ac4a3d921df80690
Mark Yen <mark.yen@suse.com>
2026-08-07 12:24:36 -0700

    RDD: extensions: implement ready condition reconciler
    This only handles the ready condition, which is derived from the other two
    conditions.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- rdd/pkg/controllers/extensions/extension/controller.go
+++ rdd/pkg/controllers/extensions/extension/controller.go
@@ -105,13 +105,13 @@ func (c *controller) setupWebhook(mgr ctrl.Manager) error {
 
 // RegisterWithManager implements the complete controller registration for both
 // embedded and external modes.  It registers the CRD types with the scheme,
-// sets up the reconciler and the validating webhook.
+// sets up the Ready reconciler and the validating webhook.
 func (c *controller) RegisterWithManager(mgr ctrl.Manager) error {
 	if err := v1alpha1.AddToScheme(mgr.GetScheme()); err != nil {
 		return err
 	}
 
-	if err := (&controllers.ExtensionReconciler{
+	if err := (&controllers.ExtensionReadyReconciler{
 		Client: mgr.GetClient(),
 	}).SetupWithManager(mgr); err != nil {
 		return err
